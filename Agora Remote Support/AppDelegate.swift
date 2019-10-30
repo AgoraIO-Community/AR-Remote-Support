b037710fab1bf07d6469b13d64929507bfe06429
Hermes <hermes.frangoudis@gmail.com>
2019-10-30 13:42:21 -0400

    create project

---
+++ Agora Remote Support/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Agora Remote Support
+//
+//  Created by Hermes Frangoudis on 10/30/19.
+//  Copyright © 2019 Agora.io. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
---
+++ Agora Remote Support/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Agora Remote Support
+//
+//  Created by digitallysavvy on 10/30/19.
+//  Copyright © 2019 Agora.io. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}

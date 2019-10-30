ba1dc8a330711b0a2446cee19d597255d426e836
Hermes <hermes.frangoudis@gmail.com>
2019-10-30 14:00:20 -0400

    added ViewController override functions

--- Agora Remote Support/AgoraRemoteSupportAudienceViewController.swift
+++ Agora Remote Support/AgoraRemoteSupportAudienceViewController.swift
@@ -10,10 +10,25 @@ import UIKit
 
 class AgoraRemoteSupportAudienceViewController: UIViewController {
 
+    override func loadView() {
+        super.loadView()
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
 
 }
--- Agora Remote Support/ViewController.swift
+++ Agora Remote Support/ViewController.swift
@@ -9,11 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    override func loadView() {
+        super.loadView()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
 }

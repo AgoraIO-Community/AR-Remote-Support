ebe309bcebbcee6b5ae0bee8a5a0b95173fb16fc
Hermes <hermes.frangoudis@gmail.com>
2019-10-30 13:58:03 -0400

    updated gitignore

---
+++ Agora Remote Support/AgoraRemoteSupportAudienceViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AgoraRemoteSupportAudienceViewController.swift
+//  Agora Remote Support
+//
+//  Created by digitallysavvy on 10/30/19.
+//  Copyright © 2019 Agora.io. All rights reserved.
+//
+
+import UIKit
+
+class AgoraRemoteSupportAudienceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}

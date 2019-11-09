570880a9e2e3ade8b130e4d8e5a8e139e11e7e9d
Hermes <hermes.frangoudis@gmail.com>
2019-11-08 22:51:48 -0500

    some helper functions

---
+++ AR Remote Support/ARVideoSource.swift
@@ -0,0 +1,9 @@
+//
+//  ARVideoSource.swift
+//  AR Remote Support
+//
+//  Created by Hermes Frangoudis on 11/8/19.
+//  Copyright © 2019 Agora.io. All rights reserved.
+//
+
+import Foundation
---
+++ AR Remote Support/SCNVector3+Extensions.swift
@@ -0,0 +1,42 @@
+//
+//  SCNVector3+Extensions.swift
+//  AR Remote Support
+//
+//  Created by Hermes Frangoudis on 11/8/19.
+//  Copyright © 2019 Agora.io. All rights reserved.
+//
+
+import SceneKit
+
+import ARKit
+
+public extension SCNVector3 {
+    func distance(to destination: SCNVector3) -> CGFloat {
+        let dx = destination.x - x
+        let dy = destination.y - y
+        let dz = destination.z - z
+        return CGFloat(sqrt(dx*dx + dy*dy + dz*dz))
+    }
+}
+
+
+// Vector Maths helper functions
+func + (left: SCNVector3, right: SCNVector3) -> SCNVector3 {
+    return SCNVector3Make(left.x + right.x, left.y + right.y, left.z + right.z)
+}
+
+func - (left: SCNVector3, right: SCNVector3) -> SCNVector3 {
+    return SCNVector3Make(left.x - right.x, left.y - right.y, left.z - right.z)
+}
+
+func * (left: SCNVector3, right: SCNVector3) -> SCNVector3 {
+    return SCNVector3Make(left.x * right.x, left.y * right.y, left.z * right.z)
+}
+
+func / (left: SCNVector3, right: SCNVector3) -> SCNVector3 {
+    return SCNVector3Make(left.x / right.x, left.y / right.y, left.z / right.z)
+}
+
+func posBetween(first: SCNVector3, second: SCNVector3) -> SCNVector3 {
+    return SCNVector3Make((first.x + second.x) / 2, (first.y + second.y) / 2, (first.z + second.z) / 2)
+}

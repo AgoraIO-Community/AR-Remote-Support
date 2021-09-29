1f5777b93ae3846e4ddcb4902dd2965ebc56b1bf
Max Cobb <maxxc@mac.com>
2021-09-29 09:29:17 +0100

    updated project to use latest swiftpm engine release

--- AR Remote SupportUITests/AR_Remote_SupportUITests.swift
+++ AR Remote SupportUITests/AR_Remote_SupportUITests.swift
@@ -8,7 +8,7 @@
 
 import XCTest
 
-class Agora_Remote_SupportUITests: XCTestCase {
+class AR_Remote_SupportUITests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
